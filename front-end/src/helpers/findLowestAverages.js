46811022e77a9929a1029557e92b5014b87667cd
Steven Sussman <sjsussman@gmail.com>
2021-03-10 15:21:01 -0500

    added explanations

--- front-end/src/helpers/findLowestAverages.js
+++ front-end/src/helpers/findLowestAverages.js
@@ -3,6 +3,7 @@ const findLowestAverages = (array) => {
 
     let holder = []
 
+    //calculate individual student average
     const studentAverage = (student)=>{
         let scoreTotal = 0;
         for(let i=0;i<student.scores.length; i++){
@@ -11,15 +12,17 @@ const findLowestAverages = (array) => {
         return scoreTotal/(student.scores.length)
       }
 
-    const getStudentAverages = (array)=>{
-        let averages = array.map((student) => {
+    //add "average" property to student object
+    const addAverages = (array)=>{
+        let updatedStudent = array.map((student) => {
           student["average"] = studentAverage(student);
           return student;
         });
-        return averages;
+        return updatedStudent;
      }
 
-    let sortedStudents = getStudentAverages(array.sort((x,y) => x.grade - y.grade))
+    //loop over students sorted by grade and compare averages
+    let sortedStudents = addAverages(array.sort((x,y) => x.grade - y.grade))
     let target = sortedStudents[0]
     for(let i=1; i<sortedStudents.length; i++){
         if (sortedStudents[i]["grade"] > target["grade"] || i === sortedStudents.length-1){
