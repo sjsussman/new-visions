84477e465d2d0b52f40440f8c6b16d376f5b8568
Steven Sussman <sjsussman@gmail.com>
2021-03-10 17:35:08 -0500

    rendered to screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,24 +8,50 @@ import findLowestAverages  from './helpers/findLowestAverages'
 function App() {
   //useState hook to set the component the state
   const [studentData, setStudentData] = useState([])
+  const [grades, setGrades] = useState()
+  const [averages, setAverages] = useState([])
 
   useEffect(() => {
   //Axios checks server @ '/studentlist' endpoint and returns the data in the response
     axios.get('/studentlist')
       .then(res => {
-      setStudentData(res.data)
-      console.log(studentData)
-    })
-    .then(res => {
-      console.log(groupByGrade(studentData));
-      console.log(findLowestAverages(studentData));
+        setGrades([groupByGrade(res.data)])
+        setAverages(findLowestAverages(res.data))
     })
       .catch(err => console.log(err));
     }, []);
 
+    let renderStudents;
+    if (grades) {
+      renderStudents = grades[0].map((student) => {
+        return <div key={student.name}>
+          <p className='name'>{student.name}</p><p className='grade'>Grade:{student.grade}</p>
+          </div>;
+      });
+    }
+
+    let renderAverage;
+    if (averages) {
+      renderAverage = averages.map((student) => {
+        return <div key={student.name}>
+          <p className='name'>{student.name}</p><p className='grade'>Grade: {student.grade}</p> <p className='average'>Grade Average: {student.average}</p>
+          </div>;
+      });
+    }
+
   return (
     <div>
-      Test
+      <h1>Steven's New Vision Performance Task</h1>
+    <div id='container'>
+      <div id='roster'> 
+      <h2>Student Roster</h2>
+        <div id='ros'>{renderStudents}</div>
+      </div>
+      <div id='lowestAverage'>
+      <h2>Lowest Average</h2>
+        <div id='avg'>{renderAverage}</div>
+      </div>
+    </div>
     </div>
   );
 }
